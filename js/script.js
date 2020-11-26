c7d14aac7b42ab6131946eb0690e96b76e7b9944
jonathon mullins <jonathonm93@gmail.com>
2020-11-26 14:48:46 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+async function getHotelData() {
+    try {
+        const response = await fetch('../hotel.json')
+        return await response.json()//return the json object
+    } catch (error) {
+        console.error(error)
+    }
+}
+let hotelData = {}
+getHotelData().then(data => hotelData = data)
+//store in a variable document.querySelector("#a")
+//use that variable to loop over each element and addeventlistener to each one i suggest using foreach
+document.querySelector("#marriott").addEventListener('click', hotelInfo)
+document.querySelector("#sheraton").addEventListener('click', hotelInfo)
+document.querySelector("#hilton").addEventListener('click', hotelInfo)
+
+function hotelInfo(event) {
+    let hotelChoice = hotelData.hotels.find(hotel => {
+        return event.target.id === hotel.name.toLowerCase()
+    })
+    document.querySelector("#hotelName").textContent = `${hotelChoice.name} hotel`
+    document.querySelector("#address").textContent = `${hotelChoice.address}`
+    document.querySelector("#rooms").textContent = `${hotelChoice.rooms}`
+    document.querySelector("#gym").textContent = `${hotelChoice.gym}`
+    document.querySelector("#roomTypes").textContent = `${hotelChoice.roomTypes}`
+    document.querySelector("#picture").textContent = `${hotelChoice.picture} image`
+
+}
