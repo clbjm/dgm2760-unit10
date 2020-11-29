73696d054330c52a001e475f7d1715ae7b042212
jonathon mullins <jonathonm93@gmail.com>
2020-11-29 13:21:54 -0700

    first commit

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ async function getHotelData() {
         console.error(error)
     }
 }
+
 let hotelData = {}
 getHotelData().then(data => hotelData = data)
 //store in a variable document.querySelector("#a")
@@ -16,13 +17,19 @@ document.querySelector("#hilton").addEventListener('click', hotelInfo)
 
 function hotelInfo(event) {
     let hotelChoice = hotelData.hotels.find(hotel => {
-        return event.target.id === hotel.name.toLowerCase()
+        return event.target.id === hotel.name.toLowerCase() 
     })
+    let image = document.createElement("img")
+    image.src= hotelChoice.picture;
     document.querySelector("#hotelName").textContent = `${hotelChoice.name} hotel`
     document.querySelector("#address").textContent = `${hotelChoice.address}`
     document.querySelector("#rooms").textContent = `${hotelChoice.rooms}`
     document.querySelector("#gym").textContent = `${hotelChoice.gym}`
     document.querySelector("#roomTypes").textContent = `${hotelChoice.roomTypes}`
-    document.querySelector("#picture").textContent = `${hotelChoice.picture} image`
-
+    document.getElementById('picture').appendChild(image)  
 }
+
+
+// let myImageTag = document.createElement("img");
+// myImageTag.src = "images/serenade.jpg";
+// document.getElementById('myCards').appendChild(myImageTag);
